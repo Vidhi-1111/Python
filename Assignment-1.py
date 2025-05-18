5de91f562d01e07162087900bb2135c230b39ed0
Vidhi-1111 <vidhibaiwarast@gmail.com>
2025-05-18 17:40:21 +0530

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,17 @@
+#Task 1
+A=int(input("Enter first number: "))
+B=int(input("Enter second number: "))
+C= A+B
+print("Addition: ", C)
+D= A-B
+print("Subtraction: ", D)
+E= A*B
+print("Multiplication: ", E)
+F= A/B
+print("Division: ", F)
+
+#Task 2
+A1= input("Enter first name: ")
+A2= input("Enter second name: ")
+B= A1+ A2
+print("Hello!",B +", Welcome to the python program.")
