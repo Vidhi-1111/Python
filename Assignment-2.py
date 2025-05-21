a227f1b3617d1978567d8a034d5d19bdd6eb1246
Vidhi-1111 <vidhibaiwarast@gmail.com>
2025-05-21 15:43:21 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,12 @@
+# Task 1
+# odd or even
+n= float(input("Enter a number: "))
+if n%2:
+    print("The given number is even")
+else:
+    print("This is an odd number")
+
+# Task 2
+for i in range(1,51):
+    print(i)
+print(" The sum of the integers is: ",sum(range(1,51)))
