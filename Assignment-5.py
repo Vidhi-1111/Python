72dc6ebeb3ddf7036599bb92c31e60a8ddd0311e
Vidhi-1111 <vidhibaiwarast@gmail.com>
2025-06-01 17:12:52 +0530

    Add files via upload

---
+++ Assignment-5.py
@@ -0,0 +1,16 @@
+# Task 1
+dict1= {'John':45, 'Helen':30, 'Michael':35}
+p= input("Enter a name: ")
+if p in dict1:
+    print(dict1[p])
+else:
+    print("The name of the student specified is not found")
+
+# Task 2
+l= [1,2,3,4,5,6,7,8,9,10]
+print(l)
+l1 = l[0:5]
+print(l1)
+l1.reverse()
+print(l1)
+
